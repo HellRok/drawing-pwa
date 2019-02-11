3150ea2998e3ab3a31c6ccd4d11d1d306890689a
Sean Earle <sean.r.earle@gmail.com>
2019-02-11 17:14:35 +1030

    Make bootstrap.sh a bit nicer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo Removing the remote
+GREEN='\033[0;32m';
+CLEAR_COLOUR='\033[0m';
+
+function green_echo {
+  echo -e "${GREEN}$1${CLEAR_COLOUR}"
+}
+
+
+green_echo 'Removing the remote'
 git remote remove origin
-echo Running bundle install
+green_echo 'Running bundle install'
 bundle install
-echo Running yarn install
+green_echo 'Running yarn install'
 yarn install
+
+green_echo 'Cleaning up files'
+rm bootstrap.sh
